67e5d0651717d806e2c7191d2a55f0015b0ab9c4
Dusan Gojkovic <dusangojkovic087gmail.com>
2021-09-20 05:10:36 -0700

    dodat navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeVideoComponent } from './components/home-video/home-video.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeVideoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
